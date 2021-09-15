659228c8577991746cdc4e73d21fab70de8e19ae
Nitish Sharma <nitss007@gmail.com>
2021-09-15 20:28:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 docker ps | grep -i "docker-test" | awk '{print $1}' | xargs docker kill || echo 'All gone'
-docker run -itd -p 8090:8080 nitss007/docker-test:$1
+img="nitss007/docker-test:$1"
+docker run -itd -p 8090:8080 $img
